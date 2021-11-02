bd300afa8372aaf3227a952acdb21fcabdcc65a3
HenriqueEduardo1 <henriqueeduardo1123@gmail.com>
2021-11-02 16:49:10 -0300

    Div. 3 02/11/2021

---
+++ Codeforces Round #753 (Div. 3)/A/a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define i64 long long
+#define sz(x) (int)(x).size()
+#define fs first
+#define sc second
+#define eb emplace_back
+#define UNVISITED 0
+#define VISITED 1
+#define INF 0x3f3f3f3f
+#define vi vector<int>
+#define vl vector<i64> 
+#define vvi vector<vi>
+#define pii pair<int,int>
+#define pll pair<i64,i64>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define sti stack<int>
+#define stii stack<pii>
+#define mii map<int,int>
+#define mll map<i64,i64>
+
+using namespace std;
+
+int n,ul;
+string t,s;
+
+int main() {_
+    cin>>n;
+
+    while(n--){
+        cin>>t>>s;
+        int res = 0;
+        for(int i = 0; i < sz(t); i++){
+            if(t[i] == s[0]){
+                ul = i;
+                break;
+            }
+        }
+        for(int i = 1; i < sz(s);i++){
+            for(int j = 0; j < sz(t); j++){
+                if(t[j] == s[i]){
+                    res += abs(ul - j);
+                    ul = j;
+                    break;
+                }
+            }
+        }
+        cout<<res<<endl;
+    }
+
+}
